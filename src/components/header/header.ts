cbc0cf8f9c5aa89466c764454f718c29e23ea9f0
MuhammedKhamis <muhammedessam10@gmail.com>
2017-07-22 19:35:47 +0200

    Header is removed, back buttons works

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -20,9 +20,9 @@ export class HeaderComponent {
   }
   goBack(){
     //just for testing ...
-    console.log(this.navCtrl);
+    //console.log(this.navCtrl);
      //This line below will be execute
-    //this.navCtrl.pop();
+    this.navCtrl.pop();
   }
 
 }
