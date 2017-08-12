93b32f1c79d6c2755d8e1c15364a4badddc6850e
Muhammed Ibrahim <muhammedessam10@gmail.com>
2017-08-12 19:11:10 +0200

    Fix2

--- src/providers/posts/posts.ts
+++ src/providers/posts/posts.ts
@@ -1,13 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
-<<<<<<< HEAD
-import {Hashtag} from "../../model/Hashtag.model";
-import {apiEndPoint} from "../../app/app.module";
-=======
 import {apiEndPoint} from "../../app/app.module";
 import { Storage } from "@ionic/storage"
->>>>>>> ionic
 
 /*
   Generated class for the PostsProvider provider.
@@ -17,20 +12,11 @@ import { Storage } from "@ionic/storage"
 */
 @Injectable()
 export class PostsProvider {
-  user: any
-
-<<<<<<< HEAD
   user: any;
   constructor(public http: Http, public storage: Storage) {
       this.storage.get('user').then(data => {
           this.user = JSON.parse(data)
       });
-=======
-  constructor(public http: Http, public storage: Storage) {
-    this.storage.get('user').then(data => {
-      this.user = JSON.parse(data)
-    })
->>>>>>> ionic
   }
 
   getData(){
@@ -42,23 +28,12 @@ export class PostsProvider {
   }
 
   sendPost(text:string, hashtagsArr: string[]){
-<<<<<<< HEAD
-        console.log(this.user)
-
-        return this.http.post(apiEndPoint + '/posts', {
-            user: this.user,
-            content: text,
-            hashtags: hashtagsArr
-  }).map(data => data.json())
-=======
     console.log(this.user)
-
     return this.http.post(apiEndPoint + '/posts', {
       user: this.user,
       content: text,
       hashtags: hashtagsArr
     }).map(data => data.json())
->>>>>>> ionic
     //TODO: Authentication
   }
 
