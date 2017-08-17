8b6dc4c4f249e3ae9ab0a689e6d29542e3e4da61
mmghannam <mohammad.mohsen@hotmail.com>
2017-08-17 19:37:18 +0200

    working push notifications functionality

--- src/providers/pusher/pusher.ts
+++ src/providers/pusher/pusher.ts
@@ -1,46 +1,44 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { ToastController } from "ionic-angular";
-import { LocalNotifications } from "@ionic-native/local-notifications";
-import { Storage } from "@ionic/storage";
+import {ToastController} from "ionic-angular";
+import {LocalNotifications} from "@ionic-native/local-notifications";
+import {Storage} from "@ionic/storage";
 
 /*
-  Generated class for the PusherProvider provider.
+ Generated class for the PusherProvider provider.
 
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular DI.
-*/
+ See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+ for more info on providers and Angular DI.
+ */
 declare var Pusher: any;
 
 @Injectable()
 export class PusherProvider {
 
-  constructor(public http: Http, private toastCtrl: ToastController, private localnotifications: LocalNotifications,
-    private storage: Storage) {
+  constructor(public http: Http,private toastCtrl: ToastController , private localnotifications: LocalNotifications,
+              private storage: Storage) {
     // Enable pusher logging - don't include this in production
     Pusher.logToConsole = true;
     let env = this;
-    var pusher = new Pusher('80504eb8c8101f00cad9', {
+    var pusher = new Pusher('a77c6db33a10e6568489', {
       cluster: 'eu',
       encrypted: true
     });
-    this.storage.set('updates', []);
+    this.storage.set('updates',[]);
     var channel = pusher.subscribe('my-channel');
-    channel.bind('my-event', function (data) {
-      console.log("Pusher data are : \n", data);
+    channel.bind('my-event', function(data) {
+      console.log("Pusher data are : \n",data);
       env.storage.get('updates')
-        .then(res => res.push(data.message))
+        .then(res=> res.push(data.message))
         .catch(e => console.log(e));
-      env.presentToast(data.message);
-      let date = new Date();
+      //uncomment this to show toast
+      // env.presentToast(data.message);
       env.localnotifications.schedule({
         id: 1,
         title: "ITW",
-        text: data.message,
-        at: date,
+        text: data.message
       });
-      env.localnotifications.getTriggeredIds().then(res => alert(JSON.stringify(res)));
     });
   }
 
